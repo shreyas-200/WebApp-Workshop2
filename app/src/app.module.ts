021366d5430ce1e926852713e508b217c180b0aa
noelneethanr <noelneethanr@unicourt.com>
2022-09-19 14:47:49 +0530

    Module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbconfig } from './config/db.config';
-import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dbconfig),
     ApiModule,
-    TestModule
   ],
   controllers: [],
   providers: [],
