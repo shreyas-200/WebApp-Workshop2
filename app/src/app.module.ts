83f4767fe2b4d52f07be5f0b67a76c6f26fc3950
noelneethanr <noelneethanr@unicourt.com>
2022-09-19 14:46:58 +0530

    gitignore

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbconfig } from './config/db.config';
+import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dbconfig),
-    ApiModule
+    ApiModule,
+    TestModule
   ],
   controllers: [],
   providers: [],
